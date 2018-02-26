aa226221088eb4580b15ff2168d41001cebed1da
jtaurm <36263455+jtaurm@users.noreply.github.com>
2018-02-26 16:23:20 +0100

    latest test script

--- Project1/Test.py
+++ Project1/Test.py
@@ -235,8 +235,145 @@ def TestP01_StringMatchExact_KMP():
 
 
 
+def TestP01_StringMatchExact_Naive():
+
+    alphabet_DNA = "tgca"
+
+    TimerSet_t1_px = CallTime.CallTime(StringMatch.StringMatch.ExactSubstring_Naive)
+    TimerSet_tx_p1 = CallTime.CallTime(StringMatch.StringMatch.ExactSubstring_Naive)
+    TimerSet_tx_px = CallTime.CallTime(StringMatch.StringMatch.ExactSubstring_Naive)
+
+    # Hold text size, var pattern size
+    print("text const, pattern var")
+    text = TestTools.TestTools.String_Random(10000, alphabet_DNA)
+    for p in range(0, 2000):
+        pattern_rnd = TestTools.TestTools.String_Random(p + 1, alphabet_DNA)
+        args = ( TestTools.TestTools.String_Inject_Random(text, pattern_rnd, 5)[:10000] , pattern_rnd)
+        res = TimerSet_t1_px.Measure( args, [len(args[0]), len(args[1])] )
+        TimerSet_t1_px.AppendToLastEntry(len(res))
+        TimerSet_t1_px.AppendToLastEntry("tcpv")
+
+        if p % 400 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    TimerSet_t1_px.Save_Append("Test ExactSubstring_Naive.csv")
+
+    # Var text size, hold pattern size
+    print("text var, pattern const")
+    pattern = TestTools.TestTools.String_Random(10, alphabet_DNA)
+    for p in range(0, 2000):
+        text_rnd = TestTools.TestTools.String_Random( (1 + p) * 10, alphabet_DNA)
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern, 5), pattern)
+        res = TimerSet_tx_p1.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_p1.AppendToLastEntry(len(res))
+        TimerSet_tx_p1.AppendToLastEntry("tvpc")
+
+        if p % 400 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    TimerSet_tx_p1.Save_Append("Test ExactSubstring_Naive.csv")
+
+    # Var text size, var pattern size
+    print("text var, pattern var")
+    for p in range(0, 2000):
+        pattern_rnd = TestTools.TestTools.String_Random(1 + p, alphabet_DNA)
+        text_rnd = TestTools.TestTools.String_Random((1 + p) * 10, alphabet_DNA)
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern_rnd, 5), pattern_rnd)
+        res = TimerSet_tx_px.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_px.AppendToLastEntry(len(res))
+        TimerSet_tx_px.AppendToLastEntry("tvpv")
+
+        if p % 400 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    TimerSet_tx_px.Save_Append("Test ExactSubstring_Naive.csv")
+
+def TestP01_StringMatchExact_Naive_Case():
+
+    alphabet_DNA = "tgca"
+    TimerSet_tx_px = CallTime.CallTime(StringMatch.StringMatch.ExactSubstring_Naive)
+
+    # best case, both vary
+    pattern_rnd = ""
+    text_rnd = ""
+
+    print("naive - best case, both vary")
+    for p in range(0, 1000):
+        pattern_rnd += "a"
+        #pattern_rnd = "aaaaaaaaaa"
+        text_rnd = "b" * (2000)
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern_rnd, 5), pattern_rnd)
+        res = TimerSet_tx_px.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_px.AppendToLastEntry(len(res))
+        TimerSet_tx_px.AppendToLastEntry("bc")
+
+        if p % 200 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    # worst case, both vary
+    pattern_rnd = ""
+    text_rnd = ""
+
+    print("naive - worst case, both vary")
+    for p in range(0, 1000):
+        pattern_rnd += "a"
+        #pattern_rnd = "aaaaaaaaaa"
+        text_rnd = "a" * (2000)
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern_rnd, 5), pattern_rnd)
+        res = TimerSet_tx_px.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_px.AppendToLastEntry(len(res))
+        TimerSet_tx_px.AppendToLastEntry("wc")
+
+        if p % 200 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    TimerSet_tx_px.Save_Append("Test ExactSubstring_Naive.csv")
+
+def TestP01_StringMatchExact_KMP_Case():
+
+    alphabet_DNA = "tgca"
+    TimerSet_tx_px = CallTime.CallTime(StringMatch.StringMatch.ExactSubstring_KMP)
+
+    # best case, both vary
+    print("kmp - best case, both vary")
+
+    for p in range(0, 1000):
+        #pattern_rnd = "aaaaaaaaaa"
+        pattern_rnd = TestTools.TestTools.String_Random(1 + p, "a")
+        #text_rnd = TestTools.TestTools.String_Random((1 + p) * 10, "b")
+        text_rnd = TestTools.TestTools.String_Random(2000, "b")
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern_rnd, 5), pattern_rnd)
+        res = TimerSet_tx_px.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_px.AppendToLastEntry(len(res))
+        TimerSet_tx_px.AppendToLastEntry("bc")
+
+        if p % 400 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    # worst case, both vary
+    print("kmp - worst case, both vary")
+    for p in range(0, 1000):
+        #pattern_rnd = "aaaaaaaaaa"
+        pattern_rnd = TestTools.TestTools.String_Random(1 + p, "a")
+        #text_rnd = TestTools.TestTools.String_Random((1 + p) * 10, "a")
+        text_rnd = TestTools.TestTools.String_Random(2000, "a")
+        args = (TestTools.TestTools.String_Inject_Random(text_rnd, pattern_rnd, 5), pattern_rnd)
+        res = TimerSet_tx_px.Measure(args, [len(args[0]), len(args[1])])
+        TimerSet_tx_px.AppendToLastEntry(len(res))
+        TimerSet_tx_px.AppendToLastEntry("wc")
+
+        if p % 400 == 0:
+            print("    p:" + str(p) + " " + str(len(args[0])))
+
+    TimerSet_tx_px.Save_Append("Test ExactSubstring_KMP.csv")
+
 #TestP01_StringMatchExact_AC()
-TestP01_StringMatchExact_KMP()
+for i in range(0,3):
+    #TestP01_StringMatchExact_KMP()
+    #TestP01_StringMatchExact_Naive()
+    print("Run " + str(i))
+    TestP01_StringMatchExact_Naive_Case()
+    #TestP01_StringMatchExact_KMP_Case()
 #TestW02_StringMatchExact()
 exit(0)
 
