902ef551e75a591cffc3e102434c7fbb4b2ce22a
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2018-04-05 11:20:45 +0200

    started BW

---
+++ Project4/search-bw.py
@@ -0,0 +1,28 @@
+from suffix_build import build_array_naive
+
+
+
+def burrows_Wheeler_search():
+	# Doing the pattern matching using the c table and the o table
+	pass
+
+
+def build_c_table(suffix_array, text):
+	# going through the suffix array and looking at the different letter.
+	c_array = []
+	for i in range(len(suffix_array)):
+		print text[i]
+		#c_array.append()
+
+
+def build_o_table():
+	# counting the number of the times 
+	# loop thorugh each of the position
+	# 2 for loops and
+	pass
+
+
+text = 'mississippi'
+suffix_array = build_array_naive('mississipi')
+
+build_c_table(suffix_array, text)
--- Project4/serach-bw.py
+++
@@ -1,6 +0,0 @@
-from suffix_build import build_array_naive
-
-
-
-def burrows_Wheeler_search():
-	pass
