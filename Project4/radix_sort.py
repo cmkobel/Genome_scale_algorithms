e5173f88b32b2af6a4d1f92785cd585f2ba2c98a
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2018-04-05 15:35:13 +0200

    constructed c table for bw

--- Project4/radix_sort.py
+++ Project4/radix_sort.py
@@ -30,26 +30,18 @@ def radix_sort(input, i):
     # Concat all non empty buckets together
 
     return done + [ b for blist in buckets for b in blist ]
-    #sorted_list = []
-    #sorted_list = done + sorted_list
-    #for bucket in buckets:
-    #    if len(bucket) != 0: #skip empty buckets
-    #        for b in bucket:
-    #            sorted_list.append(b)
 
-    #return sorted_list
+#random_strings = []
+#file = open('time_radix.csv', 'a')
+#file.write("Algorithm" + "," + "time" + "," + "n"+"\n")
+#for i in range(1, 1000000, 100):
+#    random_strings.append(''.join(random.choices(string.ascii_lowercase, k=i)))
+#    #print("Fib NOT sorted: ",random_strings)
+#    start = time.time()
+#    sorted_fib = radix_sort(random_strings, 0)
+#    end = time.time()
+#    #print("Fib Sorted: ",sorted_fib, "\n")
+#    file.write("radix_sort" + "," + str(end-start) +  ","+ str(i) + "\n")
+#
+##file.close()
 
-
-random_strings = []
-file = open('time_radix.csv', 'a')
-file.write("Algorithm" + "," + "time" + "," + "n"+"\n")
-for i in range(1, 1000000, 100):
-    random_strings.append(''.join(random.choices(string.ascii_lowercase, k=i)))
-    #print("Fib NOT sorted: ",random_strings)
-    start = time.time()
-    sorted_fib = radix_sort(random_strings, 0)
-    end = time.time()
-    #print("Fib Sorted: ",sorted_fib, "\n")
-    file.write("radix_sort" + "," + str(end-start) +  ","+ str(i) + "\n")
-
-file.close()
--- Project4/serach-bw.py
+++ Project4/serach-bw.py
@@ -9,30 +9,20 @@ def burrows_Wheeler_search():
 
 def build_c_table(suffix_array, text):
 	# going through the suffix array and looking at the different letter.
-	c_array = [0]
-	alphabet_in = []
-
-	print suffix_array
-	for i in range(1, len(suffix_array)):
-
-
-		if text[suffix_array[i]] not in alphabet_in: 
-			#print i
-			c_array.append(i-1)
-
-			alphabet_in.append(text[suffix_array[i]])
-
-	return c_array 
+	c_array = []
+	for i in suffix_array:
+		print i
+		print text[i]
+		#c_array.append()
 
 
 def build_o_table():
 	# counting the number of the times 
 	# loop thorugh each of the position
 	# 2 for loops and
-	pass
 
 
-text = 'mississippi$'
-suffix_array = build_array_naive(text)
+text = 'mississippi'
+suffix_array = build_array_naive('mississipi')
 
-print build_c_table(suffix_array, text)
+build_c_table(suffix_array, text)
